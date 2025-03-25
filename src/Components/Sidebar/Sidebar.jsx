68bda77021b50fcb0451274c7e3634971110df35
Ziyad Khan <Ziyadkhan2333@gmail.com>
2025-03-25 19:48:14 +0500

    sidebar padding changes

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -8,10 +8,10 @@ export default function Sidebar() {
         <div className="flex flex-col w-full h-full bg-white border-r">
             <div className="flex flex-col h-full justify-between">
                 <div>
-                    <div className="flex-row flex items-center pt-20 pl-5">
+                    <div className="flex-row flex items-center pt-10 pl-5">
                         <span className="font-extrabold text-xl">ChefConnect</span>
                     </div>
-                    <div className="flex-col w-full mt-20 pl-2">
+                    <div className="flex-col w-full mt-10 pl-2">
                         <div className="flex-row flex items-center hover:bg-purple-200 px-4 py-4 rounded-xl mb-1 cursor-pointer">
                             <img src={img} alt="" className="w-6" />
                             <span className="font-extrabold text-md px-4">Home</span>
